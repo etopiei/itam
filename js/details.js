14ac9346e62a09bc258508655f2d5f2d406144e7
etopiei <lj343@icloud.com>
2016-05-29 19:58:33 +1000

    Added footer
    Added footer, as well as some changes to tv.json and a new loading
    image.

--- js/details.js
+++ js/details.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-    
+
     'use strict';
-    
+
     fillDetails();
-    
+
 });
 
 function fillDetails() {
-    
+
     var query = getParameterByName("?");
     getDetailsFromQuery(query);
-    
+
 }
 
 function getParameterByName(name, url) {
@@ -24,46 +24,46 @@ function getParameterByName(name, url) {
 }
 
 function getDetailsFromQuery(queryString) {
-    
+
     $.getJSON("js/tv.json", function (json) {
-        
+
         for (var prop in json) {
-            
+
             if (!json.hasOwnProperty(prop)) {
                 //The current property is not a direct property of prop
                 console.log("prop fail");
                 continue;
             }
-            
+
             var position = queryString.indexOf(".");
             var showID = queryString.slice(0,position);
-            
+
             if (json[prop][0]["ID"] === showID) {
-                
+
                 var Genre = json[prop][0]["Genre"];
                 var Creator = json[prop][0]["Creator"];
                 var Runtime = json[prop][0]["Time"];
                 var PostURL = json[prop][0]["PosterURL"];
-                
+
                 var episodeIndex = queryString.slice(position + 1);
-                
+
                 var episodeTitle = json[prop][episodeIndex]["ShortName"];
                 var epsiodeID = json[prop][episodeIndex]["EpisodeID"];
                 var Synopsis = json[prop][episodeIndex]["Plot"];
-                
+
                 $(".epTitle").html(episodeTitle);
                 $(".Synopsis").html(Synopsis);
                 $(".Genre").html(Genre);
                 $(".Creator").html(Creator);
-                $(".Time").html(Runtime + "mins");
+                $(".Time").html(Runtime + " mins");
                 $(".poster").attr('src',PostURL);
                 document.getElementById("play").href = "play.html?=" + queryString;
-                    
-                    
+
+
                 }
-                
-            }            
-            
+
+            }
+
         });
-    
+
 }
